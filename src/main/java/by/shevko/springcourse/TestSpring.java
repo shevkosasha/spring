0ea88686b3f3dc5b10ec769a339b07848f4a1b15
shevkosasha <shevkosasha@gmail.com>
2024-08-01 09:39:42 +0300

    bean proprty scope

--- src/main/java/by/shevko/springcourse/TestSpring.java
+++ src/main/java/by/shevko/springcourse/TestSpring.java
@@ -14,10 +14,25 @@ public class TestSpring {
         Music cLassic = context.getBean("musicBeanCLassic", Music.class);
         Music rock = context.getBean("musicBeanRock", Music.class);
 
-        MusicPlayer player1 = context.getBean("musicPlayer1", MusicPlayer.class);
-        player1.playMusic();
-        MusicPlayer player2 = context.getBean("musicPlayer2", MusicPlayer.class);
-        player2.playMusic();
+        MusicPlayer player1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer player2 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        player1.setMusicList(List.of(new RockMusic(), new ClassicalMusic()));
+
+        player2.setMusicList(List.of(new Music() {
+            @Override
+            public String getSong() {
+                return "Scatman";
+            }
+        }, new ClassicalMusic()));
+        player2.setName("Specific Player");
+        player2.setVolume(4);
+
+
+        player1.playMusicList();
+        System.out.println(String.format("name: %s, volume %d", player1.getName().toUpperCase(), player1.getVolume()));
+
+        player2.playMusicList();
         System.out.println(String.format("name: %s, volume %d", player2.getName().toUpperCase(), player2.getVolume()));
 
         context.close();
